802be545b67365f2b8d4276c2f933fb3ddc40941
Victor Leschuk <vleschuk@gmail.com>
2021-01-12 11:17:58 +0300

    Formatting

--- src/email-message.cr
+++ src/email-message.cr
@@ -1,14 +1,14 @@
 require "json"
 
-struct SimpleEmail::Message                                                                
-  include JSON::Serializable                                                  
-                                                                              
-  def initialize                                                              
-  end                                                                         
-                                                                                
-  property created = Time.utc                                                 
-  property from : String = ""                                                 
-  property to = Set(String).new                                               
-  property subj : String = ""                                                 
-  property body : String = ""                                                 
-end  
+struct SimpleEmail::Message
+  include JSON::Serializable
+
+  def initialize
+  end
+
+  property created = Time.utc
+  property from : String = ""
+  property to = Set(String).new
+  property subj : String = ""
+  property body : String = ""
+end
